8706857bfe9c8c5ce72b202137c26a4d618685e6
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-03-27 00:27:25 -0400

    follow along done

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
 # lib/config.py
 import sqlite3
 
-CONN = sqlite3.connect('company.db')
-CURSOR = CONN.cursor()
+CONN = sqlite3.connect('company.db')  # is a constant equal to a hash that contains a connection to the database.
+CURSOR = CONN.cursor()   #is a constant that allows us to interact with the database and execute SQL statements
